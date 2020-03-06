b7215b87f72c1a2a10edd61b237a7f71df196073
Andrei Melis <andrei.melis@eaudeweb.ro>
2020-03-06 17:33:54 +0200

    More fields in TEAMReportAdmin

--- ozone/core/admin.py
+++ ozone/core/admin.py
@@ -213,7 +213,7 @@ class ReportingPeriodFilter(admin.SimpleListFilter):
 
 
 def related_dropdown_filter(
-    model_class, title, related_field, sort_field, sort_asc=True
+    model_class, title, related_field, display_field, sort_field, sort_asc=True
 ):
     class Wrapper(RelatedDropdownFilter):
         def __init__(self, *args, **kwargs):
@@ -222,7 +222,7 @@ def related_dropdown_filter(
             self.lookup_choices = model_class.objects.distinct().order_by(
                 f'{descending}{related_field}__{sort_field}'
             ).values_list(
-                f'{related_field}__id', f'{related_field}__name'
+                f'{related_field}__id', f'{related_field}__{display_field}'
             )
             self.title = title
     return Wrapper
@@ -233,6 +233,7 @@ def substance_dropdown_filter(model_class, related_field='substance'):
         model_class=model_class,
         title='substance',
         related_field=related_field,
+        display_field='name',
         sort_field='sort_order',
         sort_asc=True,
     )
@@ -243,6 +244,7 @@ def party_dropdown_filter(model_class, related_field='party'):
         model_class=model_class,
         title='party',
         related_field=related_field,
+        display_field='name',
         sort_field='name',
         sort_asc=True,
     )
@@ -253,8 +255,20 @@ def reporting_period_dropdown_filter(model_class, related_field='reporting_perio
         model_class=model_class,
         title='period',
         related_field=related_field,
+        display_field='name',
         sort_field='start_date',
         sort_asc=False,
+    )
+
+
+def decision_dropdown_filter(model_class, related_field='decision'):
+    return related_dropdown_filter(
+        model_class=model_class,
+        title='decision',
+        related_field=related_field,
+        display_field='decision_id',
+        sort_field='decision_id',
+        sort_asc=True,
     )
 
 
@@ -1533,13 +1547,19 @@ class TEAPIndicativeNumberOfReportsAdmin(admin.ModelAdmin):
 @admin.register(TEAPReport)
 class TEAPReportAdmin(admin.ModelAdmin):
     list_display = (
-        'sort_order', 'reporting_period', 'report_type', 'decision',
-        'report_to_be_produced',
+        'sort_order', 'reporting_period', 'report_type',
+        'issue', 'decision', 'report_to_be_produced',
     )
-    search_fields = ('reporting_period__name', 'decision__decision_id')
+    search_fields = (
+        'reporting_period__name',
+        'decision__decision_id',
+        'issue',
+    )
     list_filter = (
         'report_type',
         ReportingPeriodFilter,
+        ('issue', DropdownFilter),
+        ('decision', decision_dropdown_filter(TEAPReport))
     )
     ordering = ('sort_order',)
 
