893b3abcb95c3a14c80a55cbb5191e43ee907b5c
Andrei Melis <andrei.melis@eaudeweb.ro>
2020-05-21 18:27:12 +0300

    Cosmetic changes in labels and nav order, refs #8391

--- frontend/src/_nav.js
+++ frontend/src/_nav.js
@@ -9,17 +9,19 @@ const getNav = ($gettext) => [{
   url: '/reports'
 },
 {
-  name: $gettext('Controlled substances'),
-  url: '/lookup-tables/controlled-substances',
-  icon: 'icon-chemistry'
-}, {
-  name: $gettext('Mixtures'),
-  url: '/lookup-tables/blends',
-  icon: 'icon-layers'
-}, {
   name: $gettext('Parties'),
   url: '/lookup-tables/parties',
   icon: 'icon-people'
+},
+{
+  name: $gettext('Controlled substances'),
+  url: '/lookup-tables/controlled-substances',
+  icon: 'icon-chemistry'
+},
+{
+  name: $gettext('Mixtures'),
+  url: '/lookup-tables/blends',
+  icon: 'icon-layers'
 },
 {
   name: $gettext('Consumption / production'),
