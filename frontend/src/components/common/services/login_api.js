7a151d3b2744d0a49faceab0c8061b68c897da7a
Andrei Melis <andrei.melis@eaudeweb.ro>
2020-05-26 13:27:15 +0300

    Avoid double logout, fixes #8419

--- frontend/src/components/common/services/login_api.js
+++ frontend/src/components/common/services/login_api.js
@@ -3,7 +3,13 @@ import {
 } from './api'
 
 const removeLoginToken = () => new Promise((resolve, reject) => {
-  remove(`/auth-token/${getCookie('authToken')}/`)
+  const authcookie = getCookie('authToken')
+  // check if already logged out to prevent 401 on delete /api/auth-cookie/undefined
+  if (!authcookie) {
+    resolve()
+    return
+  }
+  remove(`/auth-token/${authcookie}/`)
     .then(() => {
       delete api.defaults.headers.authorization
       resolve()
